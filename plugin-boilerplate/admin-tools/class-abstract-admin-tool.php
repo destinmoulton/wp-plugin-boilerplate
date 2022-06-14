94abdf8b9d63eda25b6f7a3950c942cea390eddb
Destin Moulton <destin.moulton@gmail.com>
2022-06-13 20:20:30 -0600

    Added abstract admin tool class

---
+++ plugin-boilerplate/admin-tools/class-abstract-admin-tool.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PLUGIN_PACKAGE\AdminTools;
+
+/**
+ * This is the base class for the Admin Tools.
+ */
+abstract class AbstractAdminTool {
+	protected $title;
+	protected $slug;
+	protected $description;
+
+	public function __construct()
+	{
+		$this->init();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function init()
+	{
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getToolTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getToolSlug()
+	{
+		return $this->slug;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getToolDescription()
+	{
+		return $this->description;
+	}
+
+	/**
+	 * @return array ie array($this, "render");
+	 */
+	public function getToolRenderMethod()
+	{
+		return array($this, "render");
+	}
+
+	/**
+	 * @return void
+	 */
+	abstract public function render();
+}
