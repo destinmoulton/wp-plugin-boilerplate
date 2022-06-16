c219c5c11c15f2693c40e7b3c90285ae7f1e143b
Destin Moulton <destin.moulton@gmail.com>
2022-06-16 09:17:28 -0600

    Added escape text

--- plugin-boilerplate/admin/tools/example-tool/class-example-tool.php
+++ plugin-boilerplate/admin/tools/example-tool/class-example-tool.php
@@ -13,15 +13,15 @@
 namespace PLUGIN_PACKAGE\Admin\Tools;
 
 class ExampleTool extends AbstractAdminTool {
-	protected $title = "ExampleTool";
+	protected $title;
 	protected $slug = "example-tool";
-	protected $description = "Example tool that outlines the admin functionality.";
+	protected $description;
 
 	public function __construct() {
 		parent::__construct();
 
 		$this->title       = __( "Example Tool", PLUGIN_CONST_PREFIX_TEXTDOMAIN );
-		$this->description = __( "Simple debugging via console or log file.", PLUGIN_CONST_PREFIX_TEXTDOMAIN );
+		$this->description = __( "Simple example tool. Does nothing really.", PLUGIN_CONST_PREFIX_TEXTDOMAIN );
 	}
 
 	/**
