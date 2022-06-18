bd563dc104db03cef84a5c9f8d5cff443dc6deeb
Destin Moulton <destin.moulton@gmail.com>
2022-06-17 18:09:18 -0600

    Added example partial comment.

--- plugin-boilerplate/admin/tools/example-tool/partials/example-tool.partial.php
+++ plugin-boilerplate/admin/tools/example-tool/partials/example-tool.partial.php
@@ -1,2 +1,15 @@
+<?php
+
+/**
+ * Example partial template.
+ *
+ * Variables are passed in using a keyed array to
+ * the add_partial() function call in the tool class.
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/** @var $hello_world string */
+?>
 <h2>Example Tool</h2>
 <p><?= $hello_world ?></p>
